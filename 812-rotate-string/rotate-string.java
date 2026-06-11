87c868e45bb91bf67634ea8c1dda8faf3e7398d8
Nandita Bharti <nanditabharti628@gmail.com>
2026-06-11 21:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (72.69%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,22 +1,27 @@
 class Solution {
     //Brute - TC->O(N^2) , SC -> O(N)
     public boolean rotateString(String s, String goal) {
+
         if (s.length() != goal.length()) return false;
         
-        StringBuilder sb = new StringBuilder(s);
-        
-        for (int i = 0; i < s.length(); i++) {
-            // Check if current state matches goal
-            if (sb.toString().equals(goal)) {
-                return true;
-            }
+    //     StringBuilder sb = new StringBuilder(s);
+    //     for (int i = 0; i < s.length(); i++) {
+    //         // Check if current state matches goal
+    //         if (sb.toString().equals(goal)) {
+    //             return true;
+    //         }
             
-            // Simulating a shift: remove first character and append it to the back
-            char firstChar = sb.charAt(0);
-            sb.deleteCharAt(0);
-            sb.append(firstChar);
-        }
-        
-        return false;
+    //         // Simulating a shift: remove first character and append it to the back
+    //         char firstChar = sb.charAt(0);
+    //         sb.deleteCharAt(0);
+    //         sb.append(firstChar);
+    //     }
+    //     return false;
+    // }
+
+    // Optimal - TC-> O(N) , SC -> O(N)
+    String doubled  = s+s;
+    if(doubled.contains(goal)) return true;
+    return false;
     }
 }
