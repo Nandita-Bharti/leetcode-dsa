96a8c9b9e8214b0022076c3f300eccd2dd4a9bdd
Nandita Bharti <nanditabharti628@gmail.com>
2026-06-14 22:02:23 +0530

    Time: 14 ms (92.18%) | Memory: 43.7 MB (52.43%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,22 +1,48 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        String ans = "";
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                String sub = s.substring(i , j+1);
-                if(isPalinedrome(sub) && sub.length() > ans.length()){
-                    ans = sub;
-                }
+    //Brute - TC -> O(N^3)
+    //     int n = s.length();
+    //     String ans = "";
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = i; j < n; j++){
+    //             String sub = s.substring(i , j+1);
+    //             if(isPalinedrome(sub) && sub.length() > ans.length()){
+    //                 ans = sub;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+    // boolean isPalinedrome(String sub){
+    //     int left = 0 , right = sub.length() - 1;
+    //     while(left < right){
+    //         if(sub.charAt(left++) != sub.charAt(right--)) return false;
+    //     }
+    //     return true;
+    // }
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0;
+        int end = 0;  
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > (end - start + 1)) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return ans;
+        return s.substring(start, end + 1);
     }
-    boolean isPalinedrome(String sub){
-        int left = 0 , right = sub.length() - 1;
-        while(left < right){
-            if(sub.charAt(left++) != sub.charAt(right--)) return false;
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;  
+            right++; 
         }
-        return true;
+    
+        return right - left - 1;
     }
 }
