8e8b60eaa63cd1ec009a8fcbec120648e05f8ad7
Nandita Bharti <nanditabharti628@gmail.com>
2026-06-17 11:51:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,13 @@
 class Solution {
-    static int[] dp;
-    public int fib(int n) {
-        dp = new int[n+1];
-        return fibo(n);
+    public int fib(int n) {    //TC & SC = 0(n+n)
+        int[] dp = new int[n+1];
+        return fibo(n,dp);
     }
-    public int fibo(int n){
+    public int fibo(int n , int[] dp){
         if(n <= 1) return n;
         if(dp[n] != 0) return dp[n];
-        int ans = fibo(n-1) + fibo(n-2);
-        dp[n] = ans;
+        int ans = fibo(n-1 , dp) + fibo(n-2 , dp);
+        dp[n] = ans;    //at start dp[2] = 1;
         return ans;
     }
 }
