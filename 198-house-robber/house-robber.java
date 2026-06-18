c773907b187c20863ef2c2124b8fd39eaca85068
Nandita Bharti <nanditabharti628@gmail.com>
2026-06-18 18:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (73.68%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -21,8 +21,11 @@ class Solution {
         
         // dp[i] stores the max money money till house i
         int[] dp = new int[n];
+        
+        // Base cases
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
+
         for (int i = 2; i < n; i++) {
             int pick = nums[i] + dp[i - 2];
             int notPick = dp[i - 1];
