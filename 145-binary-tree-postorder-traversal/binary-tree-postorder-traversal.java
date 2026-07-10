2f95db1bc02e4398e6765600c40de08c125bfd13
Nandita Bharti <nanditabharti628@gmail.com>
2026-07-10 23:02:57 +0530

    Time: 1 ms (13.01%) | Memory: 43.1 MB (70.66%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -1,14 +1,32 @@
-
 class Solution {
-     public void dfs(TreeNode root, List<Integer> ans){
-        if(root == null) return;
-        dfs(root.left, ans);
-        dfs(root.right, ans);
-        ans.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
-          List<Integer> ans = new ArrayList<>();
-        dfs(root,ans);
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Stack<TreeNode> st1 = new Stack<>();
+        st1.push(root);
+        Stack<TreeNode> st2 = new Stack<>();
+        while(!st1.isEmpty()){
+            TreeNode curr = st1.pop();
+            st2.push(curr);
+            if(curr.left!=null) st1.push(curr.left);
+            if(curr.right!=null) st1.push(curr.right);
+        }
+        while(!st2.isEmpty()){
+            ans.add(st2.pop().val);
+        }
         return ans;
     }
 }
+// class Solution {
+//      public void dfs(TreeNode root, List<Integer> ans){
+//         if(root == null) return;
+//         dfs(root.left, ans);
+//         dfs(root.right, ans);
+//         ans.add(root.val);
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         dfs(root,ans);
+//         return ans;
+//     }
+// }
