412358bde1568ad13b4ddec8fc2c4310ea35a9b7
Nandita Bharti <nanditabharti628@gmail.com>
2026-07-13 17:42:53 +0530

    Time: 39 ms (96.76%) | Memory: 44.4 MB (48.83%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -5,35 +5,62 @@ class Solution {
         nums2[0] = 1;
         System.arraycopy(nums, 0, nums2, 1, n);
         nums2[n+1] = 1;
-        
-        int[][] memo = new int[n+2][n+2];
-        for(int i = 0; i < n+2; i++) {
-            for(int j = 0; j < n+2; j++) {
-                memo[i][j] = -1;
+        int m = n+2;
+        int[][] dp = new int[m][m];
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) continue;
+                int maxCoins = Integer.MIN_VALUE;
+                for(int k = i; k <= j; k++){
+                    int coins = nums2[i-1]*nums2[k]*nums2[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxCoins = Math.max(maxCoins , coins);
+                }
+                dp[i][j] = maxCoins;
             }
         }
-        
-        return f(1, n, nums2, memo);
-    }
-    
-    int f(int i, int j, int[] nums2, int[][] memo){
-        if(i > j) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        
-        int maxCoins = Integer.MIN_VALUE;
-        
-        for(int k = i; k <= j; k++){
-            int leftSubproblem = f(i, k - 1, nums2, memo);
-            int rightSubproblem = f(k + 1, j, nums2, memo);
-            int coinsToBurstK = nums2[i - 1] * nums2[k] * nums2[j + 1];
-            
-            int totalCoins = leftSubproblem + rightSubproblem + coinsToBurstK;
-            maxCoins = Math.max(maxCoins, totalCoins);
-        }
-        
-        return memo[i][j] = maxCoins;
+    return dp[1][n];
     }
 }
+
+
+//Memoization
+// class Solution {
+//     public int maxCoins(int[] nums) {
+//         int n = nums.length;
+//         int[] nums2 = new int[n+2];
+//         nums2[0] = 1;
+//         System.arraycopy(nums, 0, nums2, 1, n);
+//         nums2[n+1] = 1;
+        
+//         int[][] memo = new int[n+2][n+2];
+//         for(int i = 0; i < n+2; i++) {
+//             for(int j = 0; j < n+2; j++) {
+//                 memo[i][j] = -1;
+//             }
+//         }
+        
+//         return f(1, n, nums2, memo);
+//     }
+    
+//     int f(int i, int j, int[] nums2, int[][] memo){
+//         if(i > j) return 0;
+//         if(memo[i][j] != -1) return memo[i][j];
+        
+//         int maxCoins = Integer.MIN_VALUE;
+        
+//         for(int k = i; k <= j; k++){
+//             int leftSubproblem = f(i, k - 1, nums2, memo);
+//             int rightSubproblem = f(k + 1, j, nums2, memo);
+//             int coinsToBurstK = nums2[i - 1] * nums2[k] * nums2[j + 1];
+            
+//             int totalCoins = leftSubproblem + rightSubproblem + coinsToBurstK;
+//             maxCoins = Math.max(maxCoins, totalCoins);
+//         }
+        
+//         return memo[i][j] = maxCoins;
+//     }
+// }
 
 
 //Recursion
