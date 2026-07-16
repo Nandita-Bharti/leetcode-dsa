5cd9398275f07d1b6787e27a909762b3917164a2
Nandita Bharti <nanditabharti628@gmail.com>
2026-07-16 16:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (47.86%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+
+        int[] dp = new int[n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[j] = 1;
+                else{
+                  int up = 0 , left = 0;
+                  if(i > 0) up = dp[j];
+                  if(j > 0) left = dp[j-1];
+                  dp[j] = up+left;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
        // Method 1 , TC=0(2^M*N),SC=0(M-1 + N-1)
         // if(m == 1 || n == 1) return 1;
         // return uniquePaths(m-1,n) + uniquePaths(m,n-1);
@@ -17,24 +33,24 @@ class Solution {
     //     int right = f(row , col-1 , dp);
     //     return dp[row][col] = up+right;
 
-    //Space Optimization
-        int[] prev = new int[n];
-        for(int i = 0; i < m; i++){
-            int[] curr = new int[n];
-            for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0) curr[j] = 1;
-                else{
-                  int up = 0 , left = 0;
-                  if(i > 0) up = prev[j];
-                  if(j > 0) left = curr[j-1];
-                  curr[j] = up+left;
-                }
-            }
-            prev = curr;
-        }
-        return prev[n-1];
-    }
-}
+    //Space Optimization using 2 rows 
+//         int[] prev = new int[n];
+//         for(int i = 0; i < m; i++){
+//             int[] curr = new int[n];
+//             for(int j = 0; j < n; j++){
+//                 if(i == 0 && j == 0) curr[j] = 1;
+//                 else{
+//                   int up = 0 , left = 0;
+//                   if(i > 0) up = prev[j];
+//                   if(j > 0) left = curr[j-1];
+//                   curr[j] = up+left;
+//                 }
+//             }
+//             prev = curr;
+//         }
+//         return prev[n-1];
+//     }
+// }
         //Method 2
         //return paths(m-1,n-1,0,0);
         // public int paths(int row, int col, int start, int end){
