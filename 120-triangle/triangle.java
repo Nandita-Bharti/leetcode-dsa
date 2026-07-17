4405537626b32ab1a0d56634bfe6ce1b613cd330
Nandita Bharti <nanditabharti628@gmail.com>
2026-07-17 07:53:04 +0530

    Time: 3 ms (80.75%) | Memory: 45.6 MB (39.95%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,7 +1,25 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size(); 
-        int[][] dp = new int[n][n];
+        int[] prev = new int[n];
+        for(int j = 0; j < n; j++){
+            prev[j] = triangle.get(n-1).get(j);
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            int[] curr = new int[i+1];
+            for(int j = 0; j <= i; j++){
+                int down = prev[j];
+                int diagonal = prev[j+1];
+                curr[j] = triangle.get(i).get(j) + Math.min(down, diagonal);
+            }
+            prev = curr;
+        }
+        return prev[0];
+    }
+}
+
+    //Memoization
     //     return f(triangle , 0 , 0 , n , dp);
     // }
     // int f(List<List<Integer>> triangle , int i , int j , int n , int[][] dp){
@@ -14,16 +32,16 @@ class Solution {
 
     //Tabulation
     // Base Case: Initialize the bottom row of DP table with the triangle's bottom row
-        for (int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle.get(n - 1).get(j);
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                int down = dp[i + 1][j];
-                int diagonal = dp[i + 1][j + 1];
-                dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
-            }
-        }
-        return dp[0][0];
-    }
-}
+//         for (int j = 0; j < n; j++) {
+//             dp[n - 1][j] = triangle.get(n - 1).get(j);
+//         }
+//         for (int i = n - 2; i >= 0; i--) {
+//             for (int j = 0; j <= i; j++) {
+//                 int down = dp[i + 1][j];
+//                 int diagonal = dp[i + 1][j + 1];
+//                 dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
