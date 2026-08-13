ace785758cf96aec10a06859b232d85f3b1a8011
Nandita Bharti <nanditabharti628@gmail.com>
2026-08-13 23:16:52 +0530

    Time: 2 ms (52.71%) | Memory: 44.6 MB (60.60%) - LeetSync

--- 3324-split-the-array/split-the-array.java
+++ 3324-split-the-array/split-the-array.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
-        int n = nums.length;
-        int i = 0 , j = 0 , count = 0;
-        Arrays.sort(nums);
-        while(j < n){
-            if(nums[i]==nums[j]){
-                count++;
-                j++;
-                if(count > 2) return false;
-            }
-            else{
-                i = j;
-                count = 0;
-            }
-
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num , 0)+1);
+            if(map.get(num) > 2) return false;
         }
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isPossibleToSplit(int[] nums) {
+//         int n = nums.length;
+//         int i = 0 , j = 0 , count = 0;
+//         Arrays.sort(nums);
+//         while(j < n){
+//             if(nums[i]==nums[j]){
+//                 count++;
+//                 j++;
+//                 if(count > 2) return false;
+//             }
+//             else{
+//                 i = j;
+//                 count = 0;
+//             }
+
+//         }
+//         return true;
+//     }
+// }
