555b079bad0cc330965b9fba66b42f1ce5e4a1fe
Nandita Bharti <nanditabharti628@gmail.com>
2026-09-05 13:08:49 +0530

    Time: 1 ms (16.86%) | Memory: 48.7 MB (50.92%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,14 +1,49 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int x = 2;
-        int n = nums.length;
-        if(n <= 2) return n;
-        for(int j = 2; j < n; j++){
-            if(nums[j] != nums[x-2]){
-                nums[x] = nums[j];
-                x++;
+        int k =0 , n = nums.length , i = 0 , j = 0 , c = 0;
+        while(i < n && j < n){
+            if(nums[i] == nums[j]){
+                j++;
+                c++;
+            }
+            else{
+                if(c >= 2){
+                    for(int z = 0; z < 2; z++){
+                        nums[k++] = nums[i];
+                    }
+                }
+                else{
+                    nums[k++] = nums[i];
+                }
+                i = j;
+                c = 0;
             }
         }
-        return x;
+        if(c >= 2){
+            for(int z = 0; z < 2; z++){
+                nums[k++] = nums[i];
+            }
+        }else{
+            nums[k++] = nums[i];
+        }
+        
+        
+        return k;
     }
 }
+
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int x = 2;
+//         int n = nums.length;
+//         if(n <= 2) return n;
+//         for(int j = 2; j < n; j++){
+//             if(nums[j] != nums[x-2]){
+//                 nums[x] = nums[j];
+//                 x++;
+//             }
+//         }
+//         return x;
+//     }
+// }
