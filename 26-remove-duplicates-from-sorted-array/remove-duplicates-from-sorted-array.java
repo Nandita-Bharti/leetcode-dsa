d5eeddf6d7703f26b6cefc8bc72fea66edb89847
Nandita Bharti <nanditabharti628@gmail.com>
2026-09-05 12:59:33 +0530

    Time: 1 ms (77.44%) | Memory: 46.8 MB (43.05%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,12 +1,28 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int x=0;
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[x] != nums[i]){
+//                 x += 1;
+//                 nums[x] = nums[i];
+//             }
+//         }
+//         return x+1;
+//     }
+// }
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int x=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[x] != nums[i]){
-                x += 1;
-                nums[x] = nums[i];
+        int k =0 , n = nums.length , i = 0 , j = 0;
+        while(i < n && j < n){
+            if(nums[i] == nums[j]){
+                j++;
+            }
+            else{
+                nums[k++] = nums[i];
+                i = j;
             }
         }
-        return x+1;
+        nums[k++] = nums[i];
+        return k;
     }
 }
