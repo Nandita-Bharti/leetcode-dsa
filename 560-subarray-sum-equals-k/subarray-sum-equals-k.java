49cfd81d4da6d759b3d4235e14c73c4431045677
Nandita Bharti <nanditabharti628@gmail.com>
2026-09-06 18:46:01 +0530

    Time: 24 ms (75.87%) | Memory: 49.3 MB (5.55%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -5,12 +5,13 @@ class Solution {
         int prefixSum = 0;
 
         // Edge Case: A prefix sum of 0 has occurred once (representing an empty subarray)
-        map.put(0,1);
+        //map.put(0,1);
 
         for(int i = 0; i < nums.length; i++){
             prefixSum += nums[i];
             // If (prefixSum - k) exists in the map, it means there are 
             // 'n' subarrays ending here that sum to k.
+            if(prefixSum == k) count += 1;
             if(map.containsKey(prefixSum - k)) count += map.get(prefixSum - k);
 
             // Update the frequency of the current prefix sum in the map
