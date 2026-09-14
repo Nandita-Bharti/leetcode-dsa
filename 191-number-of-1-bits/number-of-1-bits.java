20cdc8203a00d594e5fcf976881630a400d6ce82
Nandita Bharti <nanditabharti628@gmail.com>
2026-09-14 09:57:31 +0530

    Time: 1 ms (29.41%) | Memory: 42.7 MB (11.68%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        // while(n != 0){
+        //     n = n & (n-1);
+        //     count++;
+        // }
+
+        for(int i = 0; i < 32; i++){
+            if((n & 1) == 1) count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
