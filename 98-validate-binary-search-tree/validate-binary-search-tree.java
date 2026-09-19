a5148c3960de75f0a38b95876d2cae41fbe3d97e
Nandita Bharti <nanditabharti628@gmail.com>
2026-09-19 17:59:40 +0530

    Time: 1 ms (16.44%) | Memory: 46.6 MB (5.28%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,17 +1,35 @@
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorder(root,ans);
-        boolean flag = true;
-        for(int i =0; i<ans.size()-1; i++){
-            if(ans.get(i) >= ans.get(i+1)) flag = false;
+    class Pair {
+        long max;
+        long min;
+        Pair(long max, long min) {
+            this.max = max;
+            this.min = min;
         }
-        return flag;
     }
-    public void inorder(TreeNode root, ArrayList<Integer> ans){
-        if(root == null) return;
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
+
+    public boolean isValidBST(TreeNode root) {
+        boolean[] isBST = new boolean[]{true};
+        maxMin(root, isBST);
+        return isBST[0];
+    }
+
+    private Pair maxMin(TreeNode root, boolean[] isBST) {
+        if (root == null) {
+            return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+        Pair lst = maxMin(root.left, isBST);
+        Pair rst = maxMin(root.right, isBST);
+
+        long data = (long) root.val;
+
+        long max = Math.max(data, Math.max(lst.max, rst.max));
+        long min = Math.min(data, Math.min(lst.min, rst.min));
+
+        if (lst.max >= data || rst.min <= data) {
+            isBST[0] = false;
+        }
+
+        return new Pair(max, min);
     }
 }
